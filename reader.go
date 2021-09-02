9d2e8c39105b430ea4e4ae34ba11345d9924c570
Brian Wang <brian@column.com>
2021-09-02 15:25:10 -0700

    Update Reader to parse tags appended by Fed

--- reader.go
+++ reader.go
@@ -112,6 +112,22 @@ func (r *Reader) parseLine() error { //nolint:gocyclo
 		return fmt.Errorf("line %q is too short for tag", r.line)
 	}
 	switch r.line[:6] {
+	case TagMessageDisposition:
+		if err := r.parseMessageDisposition(); err != nil {
+			return err
+		}
+	case TagReceiptTimeStamp:
+		if err := r.parseReceiptTimeStamp(); err != nil {
+			return err
+		}
+	case TagOutputMessageAccountabilityData:
+		if err := r.parseOutputMessageAccountabilityData(); err != nil {
+			return err
+		}
+	case TagErrorWire:
+		if err := r.parseErrorWire(); err != nil {
+			return err
+		}
 	case TagSenderSupplied:
 		if err := r.parseSenderSupplied(); err != nil {
 			return err
